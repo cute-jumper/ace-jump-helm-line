bd2889ff18d99ac2ca252fb0280d3915e645e6f8
Junpeng Qiu <qjpchmail@gmail.com>
2015-06-03 23:59:31 -0400

    Use ASCII doc instead of Org inside el file

--- ace-jump-helm-line.el
+++ ace-jump-helm-line.el
@@ -24,38 +24,73 @@
 
 ;; Demos: see https://github.com/cute-jumper/ace-jump-helm-line
 
-;; #+TITLE: ace-jump-helm-line
-;; *Ace-jump to a candidate in helm window.*
+;;                           ____________________
 
-;; This package makes use of the =avy.el=.
+;;                            ACE-JUMP-HELM-LINE
 
-;; * Setup
-;;   : (add-to-list 'load-path "/path/to/ace-jump-helm-line.el")
-;;   : (require 'ace-jump-helm-line)
-
-;;   You can use the following code to bind =ace-jump-helm-line=  (or
-;;   =ace-jump-helm-line-execute-action=. See later) to a key(say,
-;;   "C-'"):
-;;   : (eval-after-load "helm"
-;;   : '(define-key helm-map (kbd "C-'") 'ace-jump-helm-line))
+;;                               Junpeng Qiu
+;;                           ____________________
 
 
-;; * Usage
-;;   When in a helm session, for example, after you call =helm-M-x=, you can use
-;;   your key binding(for example, "C-'") to invoke =ace-jump-helm-line=. See demos.
+;; Table of Contents
+;; _________________
 
-;;   =ace-jump-helm-line= would just jump to that candidate in helm window. If you
-;;   want to jump to and automatically select the candidate, which will then quit
-;;   the helm session, you can bind the key to =ace-jump-helm-line-execute-action=.
+;; 1 Setup
+;; 2 Usage
+;; 3 Acknowledgment
 
-;;   There are two kinds of styles: avy-jump style and ace-jump-mode style. By
-;;   default, this package uses =avy-jump= style(anyway, it uses
-;;   =avy-jump.el=!). You can certainly change to =ace-jump-mode-style= by:
-;;   : (setq ace-jump-helm-line-use-avy-style nil)
 
-;; * Acknowledgment
-;;   - Thank [[https://github.com/abo-abo/avy][Oleh Krehel]] for the awesome [[https://github.com/abo-abo/avy][avy]] package.
+;; Ace-jump to a candidate in helm window.
+
+;; This package makes use of the `avy.el'.
+
+
+;; 1 Setup
+;; =======
+
+;;   ,----
+;;   | (add-to-list 'load-path "/path/to/ace-jump-helm-line.el")
+;;   | (require 'ace-jump-helm-line)
+;;   `----
+
+;;   You can use the following code to bind `ace-jump-helm-line' (or
+;;   `ace-jump-helm-line-execute-action'. See later) to a key(say, "C-'"):
+;;   ,----
+;;   | (eval-after-load "helm"
+;;   | '(define-key helm-map (kbd "C-'") 'ace-jump-helm-line))
+;;   `----
+
+
+;; 2 Usage
+;; =======
+
+;;   When in a helm session, for example, after you call `helm-M-x', you
+;;   can use your key binding(for example, "C-'") to invoke
+;;   `ace-jump-helm-line'. See demos.
+
+;;   `ace-jump-helm-line' would just jump to that candidate in helm window.
+;;   If you want to jump to and automatically select the candidate, which
+;;   will then quit the helm session, you can bind the key to
+;;   `ace-jump-helm-line-execute-action'.
+
+;;   There are two kinds of styles: avy-jump style and ace-jump-mode style.
+;;   By default, this package uses `avy-jump' style(anyway, it uses
+;;   `avy-jump.el'!). You can certainly change to `ace-jump-mode-style' by:
+;;   ,----
+;;   | (setq ace-jump-helm-line-use-avy-style nil)
+;;   `----
+
+
+;; 3 Acknowledgment
+;; ================
+
+;;   - Thank [Oleh Krehel] for the awesome [avy] package.
 ;;   - Thank @hick for the original idea.
+
+
+;;   [Oleh Krehel] https://github.com/abo-abo/
+
+;;   [avy] https://github.com/abo-abo/avy
 
 ;;; Code:
 
